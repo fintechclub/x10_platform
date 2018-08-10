de819da06173f3a8ec4366741fbe91143f5f9f75
Dmitriy Korovaev <kda.biz83@gmail.com>
2018-08-10 21:25:24 +0300

    -

--- resources/views/dashboard/one-item.blade.php
+++ resources/views/dashboard/one-item.blade.php
@@ -97,7 +97,7 @@
                 <div class="card-body">
 
                     
-                    <div id="chartdiv" width="400" height="400"></div>
+                    <canvas id="chartdiv" width="400px" height="400px"></canvas>
 
                 </div>
                 
@@ -110,99 +110,40 @@
 @endsection
 
 @section('scripts')
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <script src="/js/aWapBE.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script src="/js/aWapBE.js"></script> 
     
-    <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+    <!--<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
     <script src="https://www.amcharts.com/lib/3/pie.js"></script>
     <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+    <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>-->
 
 
 
     <script>
-        
-      
-        
-        function createChart(){
-            alert("GO");
-                var chart = AmCharts.makeChart( "chartdiv", {
-                              "type": "pie",
-                              "theme": "light",
-                              "dataProvider": [
-                                  {"asset":"Bitcoin","value":"37.08"},
-                                  {"asset":"Ripple","value":"0.31"},
-                                  {"asset":"DigiByte","value":"0.31"},
-                                  {"asset":"Bela","value":"0.03"},
-                                  {"asset":"TrustPlus","value":"0.27"},
-                                  {"asset":"BitShares","value":"0.55"},
-                                  {"asset":"GameCredits","value":"0.17"},
-                                  {"asset":"Tether","value":"56.79"},
-                                  {"asset":"NEM","value":"0.59"},
-                                  {"asset":"Synereo","value":"0.19"},
-                                  {"asset":"Steem","value":"0.55"},
-                                  {"asset":"Waves","value":"0.27"},
-                                  {"asset":"Ethereum Classic","value":"0.28"},
-                                  {"asset":"Stratis","value":"0.34"},
-                                  {"asset":"Iconomi","value":"0.24"},
-                                  {"asset":"Golem","value":"0.11"},
-                                  {"asset":"Nexium","value":"0.12"},
-                                  {"asset":"Wings","value":"0.14"},
-                                  {"asset":"Edgeless","value":"0.65"},
-                                  {"asset":"Matchpool","value":"0.17"},
-                                  {"asset":"Aragon","value":"0.44"}],
-                              "valueField": "value",
-                              "titleField": "asset",
-                              "outlineAlpha": 0.1,
-                              "depth3D": 15,
-                              "balloonText": "[[title]]<br><span style='font-size:10px'><b>[[value]]%</b></span>",
-                              "angle": 30,
-                              "export": {
-                                "enabled": false
-                              }
-                            } );
+       $(function () {
 
-                }
-          setTimeout(createChart, 5000);
-        
-             /* var chart = AmCharts.makeChart( "chartdiv1", {
-                          "type": "pie",
-                          "theme": "light",
-                          "dataProvider": {!! json_encode($chartData2) !!},
-                          "valueField": "value",
-                          "titleField": "asset",
-                          "outlineAlpha": 0.4,
-                          "depth3D": 15,
-                          "balloonText": "[[title]]<br><span style='font-size:10px'><b>[[value]]%</b></span>",
-                          "angle": 30,
-                          "export": {
-                            "enabled": false
-                          }
-                        } );
-            
-            
-            
-          new Chart(document.getElementById("piechart"), {
-                options: {
-                    responsive: true,
-                    legend: {
-                        position: 'left',
+
+              new Chart(document.getElementById("chartdiv"), {
+                    options: {
+                        responsive: true,
+                        legend: {
+                            position: 'left',
+                        }
+                    },
+                    "type": "doughnut",
+                    "data": {
+                        "labels": {!! json_encode($labels) !!},
+                        "datasets": [{
+                            "label": "",
+                            "data": {!! json_encode($chartData) !!},
+                            "backgroundColor": palette('qualitative', {{count($chartData)}}).map(function (hex) {
+                                return '#' + hex;
+                            })
+                        }],
                     }
-                },
-                "type": "doughnut",
-                "data": {
-                    "labels": {!! json_encode($labels) !!},
-                    "datasets": [{
-                        "label": "",
-                        "data": {!! json_encode($chartData) !!},
-                        "backgroundColor": palette('qualitative', {{count($chartData)}}).map(function (hex) {
-                            return '#' + hex;
-                        })
-                    }],
-                }
-            });*/
-
-        
+                });
+       }     
     </script>
 @endsection
