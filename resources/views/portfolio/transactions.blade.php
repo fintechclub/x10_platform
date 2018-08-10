1350d68f0a4b9c9a28a15b5a9c9ac4817249bdb5
Dmitriy Korovaev <kda.biz83@gmail.com>
2018-08-10 18:44:04 +0300

    nowrap

--- resources/views/portfolio/transactions.blade.php
+++ resources/views/portfolio/transactions.blade.php
@@ -17,8 +17,8 @@
                     @endcan
                 </tr>
 
-                <tr v-for="t in sortByParam(transactions,'when', 'desc')" :class="{trashed: t.deleted_at}">
-                    <td>@{{ t.when | formatDate }}</td>
+                <tr v-for="t in sortByParam(transactions, 'when', 'desc')" :class="{trashed: t.deleted_at}">
+                    <td nowrap>@{{ t.when | formatDate }}</td>
                     <td>
                         @{{ t.asset.ticker }}
                     </td>
