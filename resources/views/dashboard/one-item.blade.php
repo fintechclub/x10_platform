a1d435f3f4b8496681255626ba6adfed8b7d8c30
Dmitriy Korovaev <kda.biz83@gmail.com>
2018-08-13 10:28:09 +0300

    Revert "-"
    This reverts commit 660551faffbeb90123ac34a93dbfd3792c473fb4.

--- resources/views/dashboard/one-item.blade.php
+++ resources/views/dashboard/one-item.blade.php
@@ -74,7 +74,14 @@
                                         {{$item->asset->ticker}}
                                         ({{$item->asset->title}})
                                     </td>
-                                    <td class="text-right">{{number_format($item->amount, 5)}}</td>
+                                    <td class="text-right">
+                                        <span v-if="1==1">
+                                            {{number_format($item->amount, 1)}}
+                                        </span>
+                                        <span v-else>
+                                            {{number_format($item->amount, 5)}}
+                                        </span>
+                                    </td>
                                     <td class="text-right">{{number_format($item->getShare(),2)}} %</td>
                                 </tr>
                             @endif
