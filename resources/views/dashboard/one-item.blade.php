628a92c66378f3b28c056b44a70c8dbb809e4b35
Dmitriy Korovaev <kda.biz83@gmail.com>
2018-08-13 12:44:54 +0300

    test

--- resources/views/dashboard/one-item.blade.php
+++ resources/views/dashboard/one-item.blade.php
@@ -103,7 +103,9 @@
                 <h5 class="card-header">График распределения по долям</h5>
                 <div class="card-body">
  
-                    <canvas id="piechart" width="400px" height="400px"></canvas>
+                    <div id="chartdiv" width="600px" height="600px"></div>
+                    
+                    <!--canvas id="piechart" width="400px" height="400px"></canvas-->
 
                 </div>
             </div>
@@ -117,8 +119,56 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
     <script src="/js/aWapBE.js"></script>
     
+    <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+    <script src="https://www.amcharts.com/lib/3/pie.js"></script>
+    <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+
+
     <script>
+        var chart = AmCharts.makeChart( "chartdiv", {
+                                      "type": "pie",
+                                      "theme": "light",
+                                      "dataProvider": [
+                                          {"asset":"Bitcoin","value":"37.08"},
+                                          {"asset":"Ripple","value":"0.31"},
+                                          {"asset":"DigiByte","value":"0.31"},
+                                          {"asset":"Bela","value":"0.03"},
+                                          {"asset":"TrustPlus","value":"0.27"},
+                                          {"asset":"BitShares","value":"0.55"},
+                                          {"asset":"GameCredits","value":"0.17"},
+                                          {"asset":"Tether","value":"56.79"},
+                                          {"asset":"NEM","value":"0.59"},
+                                          {"asset":"Synereo","value":"0.19"},
+                                          {"asset":"Steem","value":"0.55"},
+                                          {"asset":"Waves","value":"0.27"},
+                                          {"asset":"Ethereum Classic","value":"0.28"},
+                                          {"asset":"Stratis","value":"0.34"},
+                                          {"asset":"Iconomi","value":"0.24"},
+                                          {"asset":"Golem","value":"0.11"},
+                                          {"asset":"Nexium","value":"0.12"},
+                                          {"asset":"Wings","value":"0.14"},
+                                          {"asset":"Edgeless","value":"0.65"},
+                                          {"asset":"Matchpool","value":"0.17"},
+                                          {"asset":"Aragon","value":"0.44"}],
+                                      "valueField": "value",
+                                      "titleField": "asset",
+                                      "outlineAlpha": 0.1,
+                                      "depth3D": 15,
+                                      "balloonText": "[[title]]<br><span style='font-size:10px'><b>[[value]]%</b></span>",
+                                      "angle": 30,
+                                      "export": {
+                                        "enabled": false
+                                      }
+                                    } );
+        
+        
+        
+        
+        
         $(function () {
+            return;
             new Chart(document.getElementById("piechart"), {
                 options: {
                     responsive: true,
